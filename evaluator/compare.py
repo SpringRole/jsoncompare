d1f2650b0b630fcb369df6daad62f1114b9fca13
Anchit Bhatia <anchitbhatia23@gmail.com>
2017-06-06 18:17:27 +0530

    added calculation of f1 score

--- evaluator/compare.py
+++ evaluator/compare.py
@@ -1,5 +1,7 @@
 import json
 
+from itertools import repeat
+from sklearn.metrics import f1_score
 from jsoncompare import jsoncompare
 
 file1 = "file1.json"  # target json
@@ -11,10 +13,13 @@ with open(file1) as f:
 with open(file2) as f:
     b = json.load(f)
 
-miss,hit=jsoncompare.are_same(a, b)[1:3]
+miss,hit,actual=jsoncompare.are_same(a, b)[1:4]
+expected=[]
+expected.extend(repeat(1, len(actual)))
 hit_val=hit[1]
 miss_val=miss[1]
 print("**********")
-print("hits = ",hit_val)
-print("miss = ",miss_val)
-print("accuracy = ",hit_val/(hit_val+miss_val)*100,"%")
+print("hits =",hit_val)
+print("miss =",miss_val)
+print("accuracy =",hit_val/(hit_val+miss_val)*100,"%")
+print("f1 score =",f1_score(expected,actual))
