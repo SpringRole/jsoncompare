f961a892cef54a59b8ed88ea46aa33e29ce01376
Anchit Bhatia <anchitbhatia23@gmail.com>
2017-06-13 14:21:45 +0530

    added batch processing

--- evaluator/compare.py
+++ evaluator/compare.py
@@ -1,25 +1,28 @@
 import json
-
+import sys
 from itertools import repeat
 from sklearn.metrics import f1_score
 from jsoncompare import jsoncompare
 
-file1 = "file1.json"  # target json
-file2 = "file2.json"  # parsed json
+def compare(a,b):
+    miss, hit, actual = jsoncompare.are_same(a, b)[1:4]
+    expected = []
+    expected.extend(repeat(1, len(actual)))
+    hit_val = hit[1]
+    miss_val = miss[1]
+    print("**********")
+    print("hits =", hit_val)
+    print("miss =", miss_val)
+    print("accuracy =", hit_val / (hit_val + miss_val) * 100, "%")
+    print("f1 score =", f1_score(expected, actual))
 
-with open(file1) as f:
-    a = json.load(f)
+if __name__=="__main__":
 
-with open(file2) as f:
-    b = json.load(f)
+    file1 = sys.argv[1]  # target json
+    file2 = sys.argv[2]  # output json
+    with open(file1) as f:
+        a = json.load(f)
+    with open(file2) as f:
+        b = json.load(f)
+    compare(a,b)
 
-miss,hit,actual=jsoncompare.are_same(a, b)[1:4]
-expected=[]
-expected.extend(repeat(1, len(actual)))
-hit_val=hit[1]
-miss_val=miss[1]
-print("**********")
-print("hits =",hit_val)
-print("miss =",miss_val)
-print("accuracy =",hit_val/(hit_val+miss_val)*100,"%")
-print("f1 score =",f1_score(expected,actual))
