b3c71ab4967697a5c3ee5aef598c3c13ac4d7f94
Anchit Bhatia <anchitbhatia23@gmail.com>
2017-07-18 20:09:03 +0530

    added documentation in jsoncompare.py

--- jsoncompare/jsoncompare.py
+++ jsoncompare/jsoncompare.py
@@ -3,101 +3,88 @@ from itertools import repeat
 
 hit = 0
 miss = 0
-actual_match=[]
+actual_match = []
+
 
 def _is_dict_same(expected, actual):
-    global hit, miss,actual_match
+    """ This function checks if 2 dictionaries are same """
+    global hit, miss, actual_match
     temp = True
     for key in expected:
         if key not in actual.keys():
-            #print(key, "not present\n")
-            if isinstance(expected[key],str):
-                miss+=1
-                print("Missing :",key,"\n")
+            if isinstance(expected[key], str):
+                miss += 1
+                print("Missing :", key, "\n")
                 actual_match.append(0)
             else:
-                miss+=len(expected[key])
-                print("Missing :",key,"\n")
-                actual_match.extend(repeat(0,len(expected[key])))
+                miss += len(expected[key])
+                print("Missing :", key, "\n")
+                actual_match.extend(repeat(0, len(expected[key])))
             temp = False
-
         else:
             are_same_flag = _are_same(expected[key], actual[key])
             if not are_same_flag:
                 pass
-                #print("different values in ", key,"\n")
-
     return temp
 
 
 def _is_list_same(expected, actual):
-    global hit, miss,actual_match
+    """ This function checks if 2 lists are same """
+    global hit, miss, actual_match
     temp = True
-    # if len(expected) > len(actual):
-    #     print("Expected : ", len(expected))
-    #     print("Actual : ", len(actual))
-    #     print("length not same\n")
-
     for i in range(len(expected)):
-        if isinstance(expected[i],(str,int)) and expected[i] in actual:
-            hit+=1
+        if isinstance(expected[i], (str, int)) and expected[i] in actual:
+            hit += 1
             actual_match.append(1)
-        elif type(expected[i])==list and expected[i] in actual:
-            if _is_list_same(expected[i],actual[actual.index(expected[i])]):
-                hit+=1
+        elif type(expected[i]) == list and expected[i] in actual:
+            if _is_list_same(expected[i], actual[actual.index(expected[i])]):
+                hit += 1
                 actual_match.append(1)
             else:
-                temp=False
+                temp = False
         elif type(expected[i]) == dict:
-            if _is_dict_same(expected[i],actual[i]):
+            if _is_dict_same(expected[i], actual[i]):
                 pass
-            #     print("dict not same")
+        # list element is not in actual
         else:
             temp = False
-            miss+=1
-            print("Missing :",expected[i], "\n")
+            miss += 1
+            print("Missing :", expected[i], "\n")
             actual_match.append(0)
-            #print("miss at", i)
     return temp
 
-def _are_same(expected, actual):
-    global hit, miss,actual_match
 
+def _are_same(expected, actual):
+    global hit, miss, actual_match
     # Ensure they are of same type
     if type(expected) != type(actual):
-        if type(expected)==str:
-            miss+=1
+        if type(expected) == str:
+            miss += 1
         else:
             miss += len(expected)
         print("type mismatch")
-        print("Expected :",type(expected))
-        print("Actual :", type(actual),"\n")
-
-        #print(expected)
+        print("Expected :", type(expected))
+        print("Actual :", type(actual), "\n")
         actual_match.extend(repeat(0, len(expected)))
-        # print("Expected : ", type(expected))
-        # print("Actual : ", type(actual))
-        # print("type mismatch\n\n")
         return False
 
     # Compare primitive types immediately
-    if isinstance(expected,(int, str, bool, float)):
-        if expected.lower().strip().replace(" ","") == actual.lower().strip().replace(" ",""):
+    if isinstance(expected, (int, str, bool, float)):
+        if expected.lower().strip().replace(" ", "") == actual.lower().strip().replace(" ", ""):
             hit += 1
             actual_match.append(1)
             return True
         else:
-            miss+=1
-            print("Expected :",expected)
-            print("Actual :",actual,"\n")
+            miss += 1
+            print("Expected :", expected)
+            print("Actual :", actual, "\n")
             actual_match.append(1)
-            #print("value mismatch\n")
             return False
 
+    # type is dictionary or list
     else:
         if isinstance(expected, dict):
             return _is_dict_same(expected, actual)
-
         if isinstance(expected, list):
             return _is_list_same(expected, actual)
 
@@ -105,10 +92,10 @@ def _are_same(expected, actual):
 
 
 def are_same(original_a, original_b):
-    global hit, miss,actual_match
+    global hit, miss, actual_match
     a = original_a
     b = original_b
-    return _are_same(a, b), ("miss", miss), ("hit", hit),actual_match
+    return _are_same(a, b), ("miss", miss), ("hit", hit), actual_match
 
 
 def json_are_same(a, b, ignore_list_order_recursively=False, ignore_value_of_keys=[]):
